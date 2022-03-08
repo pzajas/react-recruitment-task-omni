22a0fb5a28dbd722042a4a537fc0736529408434
Zajas Piotr <zajas.piotr@gmail.com>
2022-03-08 23:29:02 +0100

    Install styled-components library and apply styles to the form and list components.

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,6 +1,58 @@
 import { useState } from "react"
 import List from "./List"
 
+import styled from "styled-components"
+
+const StyledFormContainer = styled.div`
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  textarea {
+    position: relative;
+    width: 50vw;
+    resize: none;
+    border: none;
+    height: 20vh;
+    margin-bottom: 0.5rem;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  /* button {
+    position: absolute;
+    top: 25.5%;
+    right: 15.78rem;
+    
+  } */
+`
+
+const StyledButtonContainer = styled.div`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  text-align: right;
+
+  button {
+    border: none;
+    padding: 0.5rem;
+    width: 5rem;
+    margin-bottom: 0.5rem;
+  }
+`
+
+const StyledList = styled(List)`
+  width: 50vw;
+`
+
 const Form = () => {
   const [noteText, setNoteText] = useState("")
   const [notesList, setNotesList] = useState([])
@@ -17,13 +69,15 @@ const Form = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmitNote}>
+    <StyledFormContainer>
+      <StyledForm onSubmit={handleSubmitNote}>
         <textarea value={noteText} onChange={handleTextChange} />
-        <button>Add note</button>
-      </form>
-      <List notesList={notesList} setNotesList={setNotesList} />
-    </div>
+        <StyledButtonContainer>
+          <button>Add note</button>
+        </StyledButtonContainer>
+      </StyledForm>
+      <StyledList notesList={notesList} setNotesList={setNotesList} />
+    </StyledFormContainer>
   )
 }
 
---
+++ src/variables/Variables.js
@@ -0,0 +1,1 @@
+export const PrimaryColor = "#181818"

f15f3c3018283c53f7de7ee100a382e6eae95747
Zajas Piotr <zajas.piotr@gmail.com>
2022-03-08 11:49:33 +0100

    Install the react-markdown library.

--- src/components/form/Note.js
+++ src/components/form/Note.js
@@ -1,14 +1,17 @@
+import ReactMarkdown from "react-markdown"
+
 const Note = ({ note, notesList, setNotesList }) => {
   const handleDeleteNote = () => {
     setNotesList(notesList.filter(item => item.id !== note.id))
   }
 
+  const markdown = `A paragraph with *emphasis* and **strong importance**.`
   const current = new Date()
   const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`
 
   return (
     <div>
-      {note.text}
+      <ReactMarkdown children={note.text} />
       {date}
       <button onClick={handleDeleteNote}>Delete</button>
     </div>

1300bc7c8bf76cdff6585369f24e82373a13acc1
Zajas Piotr <zajas.piotr@gmail.com>
2022-03-15 12:07:21 +0100

    Fix the local storage save data issue

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,12 +1,9 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
+import { useEffect } from "react"
 import styled from "styled-components"
 import PrimaryButton from "../../elements/buttons/PrimaryButton"
-import List from "./List"
-import SingleNote from "./SingleNote"
+import Note from "./Note"
 
 const StyledFormContainer = styled.div`
-  padding: 3rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -34,23 +31,14 @@ const StyledButtonContainer = styled.div`
   text-align: right;
 `
 
-const StyledList = styled(List)`
-  width: 50vw;
-`
-
-const Form = () => {
-  const [noteText, setNoteText] = useState("")
-  const [notesList, setNotesList] = useState([])
-  const [singleNote, setSingleNote] = useState([])
-  const [isSingleNote, setIsSingleNote] = useState(false)
-
+const Form = ({ noteText, setNoteText, notesList, setNotesList, singleNote }) => {
   useEffect(() => {
     handleGetLocalStorage()
   }, [])
 
   useEffect(() => {
     handleSaveLocalStorage()
-  }, [notesList])
+  }, [notesList, singleNote])
 
   const handleTextChange = e => {
     setNoteText(e.target.value)
@@ -65,10 +53,6 @@ const Form = () => {
     } else {
       alert("Add a note pwetty pwease!")
     }
-  }
-
-  const handleIsSingleNote = () => {
-    setIsSingleNote(!isSingleNote)
   }
 
   const handleSaveLocalStorage = () => {
@@ -86,46 +70,9 @@ const Form = () => {
       <StyledForm onSubmit={handleSubmitNote}>
         <textarea placeholder="Add your note here..." value={noteText} onChange={handleTextChange} />
         <StyledButtonContainer>
-          {isSingleNote ? (
-            <Link to="/">
-              <PrimaryButton onClick={handleIsSingleNote}>Go Back</PrimaryButton>
-            </Link>
-          ) : (
-            <PrimaryButton>Add note</PrimaryButton>
-          )}
+          <PrimaryButton>Add note</PrimaryButton>
         </StyledButtonContainer>
       </StyledForm>
-      <Routes>
-        s
-        <Route
-          path="/"
-          element={
-            <div>
-              <StyledList
-                notesList={notesList}
-                singleNote={singleNote}
-                isSingleNote={isSingleNote}
-                setIsSingleNote={setIsSingleNote}
-                setNotesList={setNotesList}
-                setSingleNote={setSingleNote}
-              />
-            </div>
-          }
-        />
-        <Route
-          path="/single"
-          element={
-            <SingleNote
-              isSingleNote={isSingleNote}
-              setIsSingleNote={setIsSingleNote}
-              notesList={notesList}
-              singleNote={singleNote}
-              setSingleNote={setSingleNote}
-              setNotesList={setNotesList}
-            />
-          }
-        />
-      </Routes>
     </StyledFormContainer>
   )
 }
